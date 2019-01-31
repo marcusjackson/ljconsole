80250d2f4d18214c198e13947a5bf97712a57fe1
Marcus Jackson <marcus.fm.jackson@gmail.com>
2019-01-31 09:05:11 +0900

    add drop table and create table statements

---
+++ sql/schema.sql
@@ -0,0 +1,63 @@
+DROP TABLE IF EXISTS m_block;
+CREATE TABLE m_block (
+);
+
+DROP TABLE IF EXISTS entry;
+CREATE TABLE entry (
+);
+
+DROP TABLE IF EXISTS block;
+CREATE TABLE block (
+);
+
+DROP TABLE IF EXISTS grouping;
+CREATE TABLE grouping (
+);
+
+DROP TABLE IF EXISTS entry_grouping;
+CREATE TABLE entry_grouping (
+);
+
+DROP TABLE IF EXISTS grouping_keyword;
+CREATE TABLE grouping_keyword (
+);
+
+DROP TABLE IF EXISTS tag;
+CREATE TABLE tag (
+);
+
+DROP TABLE IF EXISTS grouping_tag;
+CREATE TABLE grouping_tag (
+);
+
+DROP TABLE IF EXISTS tag_keyword;
+CREATE TABLE tag_keyword (
+);
+
+DROP TABLE IF EXISTS nested_tag;
+CREATE TABLE nested_tag (
+);
+
+DROP TABLE IF EXISTS location;
+CREATE TABLE location (
+);
+
+DROP TABLE IF EXISTS entity;
+CREATE TABLE entity (
+);
+
+DROP TABLE IF EXISTS entity_location;
+CREATE TABLE entity_location (
+);
+
+DROP TABLE IF EXISTS purpose;
+CREATE TABLE purpose (
+);
+
+DROP TABLE IF EXISTS subject;
+CREATE TABLE subject (
+);
+
+DROP TABLE IF EXISTS time;
+CREATE TABLE time (
+);
